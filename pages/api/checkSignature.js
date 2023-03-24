dc7715f433c9149cba85d937f3c9bbade30f11f9
nithilan4 <72151847+slip1244@users.noreply.github.com>
2023-03-24 15:20:58 -0500

    efficiency upgrade

--- pages/api/checkSignature.js
+++ pages/api/checkSignature.js
@@ -4,12 +4,8 @@ import { faunaClient } from '../../util/fauna';
 export default async (req, res) => {
     if (req.method == 'GET') {		
         let query = await faunaClient.query(
-			
-			q.Map(
-				q.Paginate(q.Documents(q.Collection("operating-agreement-signatures")), {size: 100000}),
-    			q.Lambda("X", q.Select(["data", "address"], q.Get(q.Var("X")), ""))
-			)
+			q.Exists(q.Match(q.Index("address"), req.query.address.toLowerCase()))
         );
-        res.status(200).json(query.data.map(a => a.toLowerCase()).includes(req.query.address.toLowerCase()));
+        res.status(200).json(query);
     }
 };
