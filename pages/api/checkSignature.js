31af52fd070b33ae87b99c666a504cdcfb7dcdbb
nithilan4 <72151847+slip1244@users.noreply.github.com>
2023-03-31 20:56:16 -0500

    fix lowercase bug

--- pages/api/checkSignature.js
+++ pages/api/checkSignature.js
@@ -4,7 +4,7 @@ import { faunaClient } from '../../util/fauna';
 export default async (req, res) => {
     if (req.method == 'GET') {		
         let query = await faunaClient.query(
-			q.Exists(q.Match(q.Index("address"), req.query.address.toLowerCase()))
+			q.Exists(q.Match(q.Index("address"), req.query.address))
         );
         res.status(200).json(query);
     }
